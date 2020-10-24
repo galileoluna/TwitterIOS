1cfa573f70cda43b17e79b9a4a82f3bbf18e0f6e
Galileo Luna <galileoluna@iMac-de-Galileo.local>
2020-10-24 15:24:43 -0300

    ordeno carpetas

---
+++ Twitter/Controller/MainTabController.swift
@@ -0,0 +1,21 @@
+//
+//  MainTabController.swift
+//  Twitter
+//
+//  Created by Galileo Luna on 24/10/2020.
+//  Copyright © 2020 Galileo Luna. All rights reserved.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+  
+}
